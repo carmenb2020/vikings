6b88ff55b808cca96c5031398c1baebcffa81a65
carmenb2020 <carmenborjap@hotmail.com>
2023-07-11 12:15:57 +0200

    finished

--- src/saxon/Saxon.js
+++ src/saxon/Saxon.js
@@ -1,3 +1,24 @@
-class Saxon {}
+const Soldier = require("../soldier/Soldier");
+
+class Saxon extends Soldier{
+    constructor(health, strength) {
+        super(health, strength);
+        this.health = health;
+        this.strength = strength;
+    };
+
+    attack() {
+        return (this.strength);
+    };
+
+    receiveDamage(damage) {
+        this.health = this.health - damage;
+        if (this.health > 0) {
+            return (`A Saxon has received ${damage} points of damage`);
+        }
+        else return ("A Saxon has died in combat");
+    };
+
+}
 
 module.exports = Saxon;
--- src/war/War.js
+++ src/war/War.js
@@ -1,3 +1,50 @@
-class War {}
+const Soldier = require("../soldier/Soldier");
 
-module.exports = War;
+class War {
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    };
+
+    addViking(viking) {
+        this.vikingArmy.push(viking);
+    };
+
+    addSaxon(saxon) {
+        this.saxonArmy.push(saxon);
+    };
+
+    vikingAttack() {
+        const vikingRandom = Math.floor(Math.random() * this.vikingArmy.length);
+        const saxonRandom = Math.floor(Math.random() * this.saxonArmy.length);
+        const vikingSoldier = this.vikingArmy[vikingRandom];
+        const saxonSoldier = this.saxonArmy[saxonRandom];
+        saxonSoldier.receiveDamage(vikingSoldier.strength);
+        if (saxonSoldier.health <= 0) {
+            this.saxonArmy.splice(saxonRandom, 1);
+            return ("A Saxon has died in combat");
+        };
+        return("saxonSoldier.health");
+    }
+    
+    saxonAttack() {
+        const vikingRandom = Math.floor(Math.random() * this.vikingArmy.length);
+        const saxonRandom = Math.floor(Math.random() * this.saxonArmy.length);
+        const vikingSoldier = this.vikingArmy[vikingRandom];
+        const saxonSoldier = this.saxonArmy[saxonRandom];
+        vikingSoldier.receiveDamage(saxonSoldier.strength);
+        if (vikingSoldier.health <= 0) {
+            this.vikingArmy.splice(vikingRandom, 1);
+            //return ("A Saxon has died in combat");
+        };
+        return(`${vikingSoldier.name} has received ${saxonSoldier.strength} points of damage`);
+    }
+
+    showStatus() {
+        if (this.saxonArmy.length == 0) {
+            return ("Vikings have won the war of the century!");
+        }
+    }
+}
+
+ module.exports = War;
